41efe2fe962cada98654269a5101c13c3b6e4e82
Sam Barnes <sam@avantrural.com>
2014-03-06 11:01:06 +0000

    add sbFreeTokenInput which extends sbTokenInput

--- src/token-input.js
+++ src/token-input.js
@@ -1,3 +1,4 @@
+/* jshint es3: true, browser: true */
 
 angular.module('sbTokenInput', ['sbMeasureText'])
 
@@ -9,14 +10,14 @@ angular.module('sbTokenInput', ['sbMeasureText'])
       scope: {
         tokens:'=sbTokens',
         input: '=sbInput',
-        formatDisplay: '=sbFormatDisplay',
-        displayProperty: '=sbDisplayProperty',
+        formatDisplay: '&sbFormatDisplay',
+        displayProperty: '&sbDisplayProperty'
       },
       template: [
         '<div class="token-input" ng-click="onClick($event)">',
           '<span class="token-input-token" ng-repeat="token in tokens" tabindex="0" ng-keydown="onTokenKeydown($event, $index)">',
             '<span class="token-input-token-name" ng-bind="formatDisplay(token)"></span>',
-            '<a class="token-input-token-remove" ng-click="removeToken(token)"></a>',
+            '<a class="token-input-token-remove" ng-click="removeTokenAt(index)"></a>',
           '</span>',
           '<input class="token-input-input" type="text" ng-model="input" ng-style="inputStyle" ng-keydown="onInputKeydown($event)">',
         '</div>'
@@ -25,8 +26,9 @@ angular.module('sbTokenInput', ['sbMeasureText'])
       link: function(scope, elem, attrs, model) {
         var input = elem.find('.token-input-input');
 
-        if (!scope.tokens) scope.tokens = [];
-
+        scope.formatDisplay = scope.formatDisplay();
+        scope.displayProperty = scope.displayProperty();
+        scope.tokens = scope.tokens || [];
         scope.inputStyle = { width: '10px' };
 
         if (!scope.formatDisplay) {
@@ -41,8 +43,8 @@ angular.module('sbTokenInput', ['sbMeasureText'])
           }
         }
 
-        scope.removeToken = function (token) {
-          var index = scope.tokens.indexOf(token);
+        scope.removeTokenAt = function (index) {
+          // Remove token
           scope.tokens.splice(index, 1);
           // Set the focus to the next token back
           $timeout(function () {
@@ -113,4 +115,53 @@ angular.module('sbTokenInput', ['sbMeasureText'])
         scope.$watch('input', scope.resizeInput);
       }
     };
+  }])
+
+  .directive('sbFreeTokenInput', ['sbTokenInputDirective', function (directive) {
+
+    var def = {}, parent = directive[0];
+
+    // Copy the parent directive definition
+    angular.extend(def, parent);
+
+    def.require = 'ngModel';
+
+    def.scope = {
+      formatDisplay: '&sbFormatDisplay',
+      displayProperty: '&sbDisplayProperty'
+    };
+
+    // Override link function
+    def.link = function (scope, elem, attrs, ctrl) {
+      // Call parent link function
+      parent.link.call(this, scope, elem, attrs);
+
+      // Watch the input and add token when a comma is entered
+      scope.$watch('input', function (text) {
+        if (!text) return;
+        if (text.charAt(text.length-1) === ',') {
+          var token = text.substr(0, text.length-1);
+          if (~scope.tokens.indexOf(token)) return;
+          scope.tokens.push(token);
+          scope.input = '';
+        }
+      });
+
+      scope.$watch('tokens', function (val, prev) {
+        console.log('watch', val, prev);
+        ctrl.$setViewValue(val);
+      }, true);
+
+      ctrl.$render = function () {
+        if (ctrl.$viewValue) {
+          scope.tokens = ctrl.$viewValue;
+        }
+      };
+    };
+
+    // Delete compile function else the new link function wont be called
+    delete def.compile;
+
+    // Return the definition
+    return def;
   }]);
